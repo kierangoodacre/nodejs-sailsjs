1a9ab1a2734f5d76d18739a60d711758ae157105
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-04-11 11:52:11 +0100

    adding sails app files

---
+++ assets/js/spa.js
@@ -0,0 +1,21 @@
+angular.module('PLatzi', []);
+
+angular.module('PLatzi').controller('BaseCtrl', ['$scope', function($scope){
+
+  io.socket.get('/emoji', function (data){
+    $scope.emojis = data;
+    $scope.$apply();
+  });
+
+  io.socket.on('emoji', function(event){
+    switch (event.verb) {
+      case 'created':
+        $scope.emojis.push(event.data);
+        $scope.$apply();
+        break;
+    }
+  });
+
+
+
+}]);
